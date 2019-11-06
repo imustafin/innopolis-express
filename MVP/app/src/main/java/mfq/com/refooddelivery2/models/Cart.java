6079f2bc675e9f6695cb94d61a6fe7e6804c49ec
Batyr Seven <batyrseven@gmail.com>
2019-11-07 00:00:09 +0300

    Issue #4: The products of the same type can be added to Cart multiple times Expected behavior: The products of the same type cannot be added to Cart multiple times. Instead, the quantity of the product is increased. Actual behavior: The products of the same type can be added to Cart multiple times. The quantity of the product is not changing.

--- MVP/app/src/main/java/mfq/com/refooddelivery2/models/Cart.java
+++ MVP/app/src/main/java/mfq/com/refooddelivery2/models/Cart.java
@@ -1,8 +1,13 @@
 package mfq.com.refooddelivery2.models;
 
 
+import android.os.Build;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import androidx.annotation.RequiresApi;
 
 public class Cart {
 
@@ -23,9 +28,15 @@ public class Cart {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void addProduct(Product product) {
-        if (!mProducts.contains(product))
+        Optional<Product> temp = mProducts.stream().filter(x -> x.getName().equals(product.getName())).findFirst();
+        if (temp.isPresent()){
+            Product tempProduct = temp.get();
+            mProducts.get(mProducts.indexOf(tempProduct)).setQuantity(tempProduct.getQuantity() + product.getQuantity());
+        } else {
             mProducts.add(product);
+        }
     }
 
     public List<Product> getProducts() {
