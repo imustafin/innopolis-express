3b308796fb4a1fa3c0feaeb0a551bd26bc06fe14
laskaa <32509071+laskaa@users.noreply.github.com>
2019-11-15 20:16:59 +0300

    Updating user profile after regestration

--- MVP/app/src/main/java/mfq/com/refooddelivery2/activity/SignUpActivity.java
+++ MVP/app/src/main/java/mfq/com/refooddelivery2/activity/SignUpActivity.java
@@ -10,10 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -191,6 +196,19 @@ public class SignUpActivity extends AppCompatActivity {
                         // Sign in success, update UI with the signed-in user's information
                         status = RequestStatus.SUCCESS;
                         Log.d("INFO", "createUserWithEmail:success");
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(mName).build();
+
+                        user.updateProfile(profileUpdates)
+                            .addOnCompleteListener(task1 -> {
+                                if (task1.isSuccessful()) {
+                                    Log.d("INFO", "User profile updated.");
+                                }
+                            });
+
                         result.set(true);
                     } else {
                         // If sign in fails, display a message to the user.
